159fc52a076656e3e84de8de6de0c1a32d83e4d6
Ryan B <admirallasers@gmail.com>
2017-01-25 17:59:21 -0600

    task controller

--- BangazonTaskTracker/Controllers/TaskController.cs
+++ BangazonTaskTracker/Controllers/TaskController.cs
@@ -1,89 +1,31 @@
-﻿using System;
+﻿using BangazonTaskTracker.DAL;
+using BangazonTaskTracker.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 
 namespace BangazonTaskTracker.Controllers
 {
-    public class TaskController : Controller
+    public class TaskController : ApiController
     {
+        TrackerRepo repo = new TrackerRepo();
+        public IHttpActionResult Post([FromBody]TaskInput new_task)
+        {
+            return Ok();
+        }
+
         // GET: Task
-        public ActionResult Index()
+        public IEnumerable<TrackerTask> Get()
         {
-            return View();
+            return null;
         }
 
-        // GET: Task/Details/5
-        public ActionResult Details(int id)
+        public IHttpActionResult Put([FromBody]TaskInput put)
         {
-            return View();
-        }
-
-        // GET: Task/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Task/Create
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Task/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Task/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Task/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Task/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
+            return Ok();
         }
     }
 }
